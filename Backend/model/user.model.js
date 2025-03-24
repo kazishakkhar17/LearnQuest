f133e5ec315768412bb60caca18b2a76d54e2c0c
phigratio <muqtadir.mujahid02@gmail.com>
2025-03-24 14:23:49 +0600

    added model files

---
+++ Backend/model/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema=mongoose.Schema({
+  FullName:{
+    type:String,
+    required:true
+  },
+  Email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  Password:{
+    type:String,
+    required:true
+  },
+
+ 
+})
+
+const User=mongoose.model("User",userSchema);
+export default User;
