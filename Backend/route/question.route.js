81fd8130ea36fd844298fa0f2532e2e283824d7a
kazishakkhar17 <kazishakkhar04@gmail.com>
2025-04-27 02:53:10 +0600

    Added Deletion,Improved Exam formats,All working

--- Backend/route/question.route.js
+++ Backend/route/question.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getQuestion, addQuestion } from "../controller/question.controller.js"; // Importing controller functions
-
+import { deleteQuestion } from "../controller/question.controller.js"; // Importing delete function
 const router = express.Router();
 
 // Route to get all questions
@@ -8,5 +8,7 @@ router.get("/", getQuestion);
 
 // Route to add a new question
 router.post("/", addQuestion);
+
+router.delete('/:id', deleteQuestion);
 
 export default router;
